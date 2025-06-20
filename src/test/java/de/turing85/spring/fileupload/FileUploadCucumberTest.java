8ce7b996ce2302b09d0529ac5d158cbb30076902
Marco Bungart <marco.bungart@googlemail.com>
2025-06-20 12:21:55 +0200

    Fix tests to not log errors

--- src/test/java/de/turing85/spring/fileupload/FileUploadCucumberTest.java
+++ src/test/java/de/turing85/spring/fileupload/FileUploadCucumberTest.java
@@ -1,15 +1,9 @@
 package de.turing85.spring.fileupload;
 
-import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 
 @Suite
 @SelectClasspathResource("de/turing85/spring/fileupload")
-@CucumberContextConfiguration
-@ContextConfiguration(classes = FileUploadApplication.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class FileUploadCucumberTest {
+public class FileUploadCucumberTest {
 }
